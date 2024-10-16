f8387f57490b0c081f95f730202258ee395b55fc
Oleg Shpaner <oleg.shpaner@joyrideautos.com>
2024-10-16 17:06:07 +0200

    third commit

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,14 +1,33 @@
 import {Card, CardDescription, CardHeader, CardTitle} from '@/components/ui/card';
 
-export default function Test() {
+const fetchPosts = async () => {
+    // Fetch data from an external API (or it could be a database, etc.)
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+    // Ensure the fetch was successful
+    if (!res.ok) {
+        throw new Error('Failed to fetch data');
+    }
+
+    // Parse the response as JSON
+    const posts = await res.json();
+
+    // Limit the number of posts to 5
+    return posts.slice(0, 4);
+};
+
+export default async function Test() {
+    const posts = await fetchPosts();
     return (
-        <div className="flex justify-center pt-5">
-            <Card>
-                <CardHeader>
-                    <CardTitle>Hello world!</CardTitle>
-                    <CardDescription>This is server side component</CardDescription>
-                </CardHeader>
-            </Card>
+        <div className="grid grid-cols-4 gap-4">
+            {posts.map((post: {id: string; title: string; body: string}) => (
+                <Card key={post.id}>
+                    <CardHeader>
+                        <CardTitle>{post.title}</CardTitle>
+                        <CardDescription>{post.body}</CardDescription>
+                    </CardHeader>
+                </Card>
+            ))}
         </div>
     );
 }
